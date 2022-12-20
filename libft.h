8510d69c14cae50ac3cafacbe4589acf472e4996
Imanol De La Iglesia Muriel <ide-la-i@c2r1s2.42urduliz.com>
2022-12-20 16:32:45 +0100

    todo menos split

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: ide-la-i <ide-la-i@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/02 12:35:42 by ide-la-i          #+#    #+#             */
-/*   Updated: 2022/12/20 15:36:53 by ide-la-i         ###   ########.fr       */
+/*   Updated: 2022/12/20 16:21:55 by ide-la-i         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,5 +49,7 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 void	ft_striteri(char *s, void (*f)(unsigned int, char*));
 void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
+void	ft_putendl_fd(char *s, int fd);
+void	ft_putnbr_fd(int n, int fd);
 
 #endif
