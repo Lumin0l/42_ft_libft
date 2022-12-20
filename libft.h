1f4e1845426d1091f5ce2200158c450cd1faeecf
Imanol De La Iglesia Muriel <ide-la-i@c2r1s2.42urduliz.com>
2022-12-20 17:17:42 +0100

    final

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: ide-la-i <ide-la-i@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/02 12:35:42 by ide-la-i          #+#    #+#             */
-/*   Updated: 2022/12/20 16:21:55 by ide-la-i         ###   ########.fr       */
+/*   Updated: 2022/12/20 16:33:56 by ide-la-i         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,5 +51,6 @@ void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
+char	**ft_split(char const *s, char c);
 
 #endif
